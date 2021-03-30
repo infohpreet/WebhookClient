2a6d0b7512bfb0b18407c8963b84fabb3eda2b96
Harpreet Singh <harpreet@elkjop.no>
2021-03-30 15:30:59 +0200

    Add project files.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var postData = DataStore.ReadFromDisk();
+
+            var output = string.Empty;
+
+            var list = postData.Items;
+
+            list.Reverse();
+
+            var counter = 0;
+
+            list.ForEach(item => { item.Id = counter++; });
+
+            ViewBag.Output = list;
+
+            return View();
+        }
+
+        [Route("/post")]
+        [HttpPost]
+        public HttpStatusCode Post()
+        {
+            var data = string.Empty;
+
+            var memoryStream = new MemoryStream();
+
+            Request.Body.CopyToAsync(memoryStream);
+
+            data = ASCIIEncoding.UTF8.GetString(memoryStream.ToArray());
+
+            if (!String.IsNullOrWhiteSpace(data))
+            {
+                var postData = DataStore.ReadFromDisk();
+
+                var headers = new List<KeyValue>();
+
+                foreach (var header in Request.Headers.ToList())
+                {
+                    headers.Add(new KeyValue(header.Key, header.Value));
+                }
+
+                postData.Items.Add(new DataItem { Body = data, Headers = headers, TimeStamp = DateTime.Now });
+
+                if (postData.Items.Count > 50)
+                {
+                    postData.Items = postData.Items.Skip(postData.Items.Count - 50).ToList();
+                }
+
+                DataStore.WriteToDisk(postData);
+            }
+
+            return HttpStatusCode.OK;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/DataStore.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Web
+{
+    [Serializable]
+    public class DataStore
+    {
+        private static string DataStoreFile
+        {
+            get
+            {
+                var path = System.AppDomain.CurrentDomain.BaseDirectory + @"\DataStore.xml";
+
+                return path;
+            }
+        }
+
+        public List<DataItem> Items { get; set; }
+
+        public DataStore()
+        {
+            Items = new List<DataItem>();
+        }
+
+        public string ToXml()
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(this.GetType());
+
+                using (var textWriter = new StringWriter())
+                {
+                    xmlSerializer.Serialize(textWriter, this);
+
+                    return textWriter.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static DataStore FromXml(string xml)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(DataStore));
+
+                var textReader = new StringReader(xml);
+
+                return (DataStore)xmlSerializer.Deserialize(textReader);
+            }
+            catch
+            {
+                return new DataStore();
+            }
+        }
+
+        public static void WriteToDisk(DataStore data)
+        {
+            File.WriteAllText(DataStoreFile, data.ToXml());
+        }
+
+        public static DataStore ReadFromDisk()
+        {
+            return !File.Exists(DataStoreFile)
+                ? new DataStore()
+                : DataStore.FromXml(File.ReadAllText(DataStoreFile));
+        }
+    }
+
+    [Serializable]
+    public class DataItem
+    {
+        public DataItem()
+        {
+            Headers = new List<KeyValue>();
+
+            Body = string.Empty;
+
+            TimeStamp = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public List<KeyValue> Headers { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+
+    [Serializable]
+    public class KeyValue
+    {
+        public KeyValue()
+        {
+        }
+
+        public KeyValue(string key, string value)
+        {
+            Key = key;
+
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
